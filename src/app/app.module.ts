da452d1ef167ea2ff1777a7b0f74960fee2a80ae
MarkyMark33 <holden_mark33@hotmail.com>
2017-11-09 16:07:11 -0600

    Get rid of app-routing.module and use ng generate component to add in new pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,37 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes, Router } from '@angular/router'; 
+import { FormsModule  } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+//  { path: 'our-pricing', component: OurPricingComponent, data: { title: "Meek Construction Pricing" } },
+  { path: '**', component: PageNotFoundComponent }
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(
+      routes,
+      {enableTracing: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
